8133e26a4f39c704a9a58fc26fa0098328134ea1
BryanMureithi <brymureithi@gmail.com>
2023-08-14 20:05:13 +0300

    insertion and deletion

--- linked_list.py
+++ linked_list.py
@@ -46,6 +46,62 @@ class LinkedList:
         new_node.next_node = self.head
         self.head = new_node
 
+    def search(self, key):
+        """
+        Search for the first node containing data that matches the key
+        Return the node or None if not found
+
+        Takes O(n) time
+        """
+
+        current = self.head
+
+        while current:
+            if current.data == key:
+                return current
+            else:
+                current = current.next_node
+        return None 
+
+    def insert(self, data, index):
+        if index == 0:
+            self.add(data)
+
+        if index > 0:
+            new = Node(data)
+
+            position = index
+            current = self.head
+
+            while position > 1:
+                current = node.next_node
+                position -= 1
+
+                prev_node = current  
+                next_node =  current.next_node
+
+                prev_node.next_node = new
+                new.next_node = next_node     
+
+
+    def remove(self, key):
+      current = self.head
+      previous = None
+      found = False     
+
+      while current and not found:
+          if current.data == key and current is self.head:
+              found = True
+              self.head = current.next_node
+          elif current.data == key:
+              found = True
+              previous.next_node = current.next_node
+          else:
+              previous = current
+              current = current.next_node
+
+      return current                               
+
     def __repr__(self):
         """
         Return a string representation of the list
@@ -64,3 +120,13 @@ class LinkedList:
                 nodes.append("[%s]" % (current.data))
 
             current = current.next_node    
+        return '-> '.join(nodes)    
+    
+
+l = LinkedList()
+l.add(10)
+l.add(25)
+l.add(46)
+n = l.search(25)
+print(n)
+print(l)
